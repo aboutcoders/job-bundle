a6ae759ae75a218f6f84b58534da2bfc96af1a33
Hannes Schulz <schulz@daten-bahn.de>
2016-09-03 11:40:49 +0200

    make AbcProcessControlBundle optional

---
+++ DependencyInjection/Compiler/ControllerConfigurationPass.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* This file is part of the job-bundle package.
+*
+* (c) Hannes Schulz <hannes.schulz@aboutcoders.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Abc\Bundle\JobBundle\DependencyInjection\Compiler;
+
+/**
+ * @author Hannes Schulz <hannes.schulz@aboutcoders.com>
+ */
+class ControllerConfigurationPass
+{
+
+}
